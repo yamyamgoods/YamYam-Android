5f82f48c8e7498da0c85073ded079b3c87eb0915
chogyujin <gyujin7@gmail.com>
2019-06-26 22:03:37 +0900

    tab layout and move fragment

--- YamYam_Android/app/src/main/java/org/yamyamgoods/yamyam_android/MainActivity.kt
+++ YamYam_Android/app/src/main/java/org/yamyamgoods/yamyam_android/MainActivity.kt
@@ -2,11 +2,74 @@ package org.yamyamgoods.yamyam_android
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentTransaction
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setOnClickListener()
+
+        startView()
+    }
+    fun startView(){
+        //TODO 처음 뷰 넣어주세요
+//        addFragment()
+        iv_bottom_best_icon.isSelected=true
+        tv_bottom_best_text.isSelected=true
+        setOnClickListener()
+    }
+    fun setOnClickListener(){
+        btn_bottom_best.setOnClickListener{
+            //TODO 프래그먼트 넣어주세요
+//            replaceFragment()
+            clearBtnSelect()
+            iv_bottom_best_icon.isSelected=true
+            tv_bottom_best_text.isSelected=true
+        }
+        btn_bottom_store.setOnClickListener{
+//            replaceFragment()
+            clearBtnSelect()
+            iv_bottom_store_icon.isSelected=true
+            tv_bottom_store_text.isSelected=true
+        }
+        btn_bottom_goods.setOnClickListener{
+//            replaceFragment()
+            clearBtnSelect()
+            iv_bottom_goods_icon.isSelected=true
+            tv_bottom_goods_text.isSelected=true
+        }
+        btn_bottom_like.setOnClickListener{
+//            replaceFragment()
+            clearBtnSelect()
+            iv_bottom_like_icon.isSelected=true
+            tv_bottom_like_text.isSelected=true
+        }
+    }
+
+    fun clearBtnSelect(){
+        iv_bottom_best_icon.isSelected=false
+        tv_bottom_best_text.isSelected=false
+        iv_bottom_store_icon.isSelected=false
+        tv_bottom_store_text.isSelected=false
+        iv_bottom_goods_icon.isSelected=false
+        tv_bottom_goods_text.isSelected=false
+        iv_bottom_like_icon.isSelected=false
+        tv_bottom_like_text.isSelected=false
+    }
+
+    fun replaceFragment(fragment:Fragment){
+        val transaction:FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fl_main_fragment,fragment)
+        transaction.commit()
+    }
+    fun addFragment(fragment: Fragment){
+        val transaction:FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.fl_main_fragment,fragment)
+        transaction.commit()
     }
 }
