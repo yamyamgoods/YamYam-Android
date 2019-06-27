4566240fbefdc28bedfbda39e523b6cac4739f8c
Malibin <nightmare73@naver.com>
2019-06-27 15:47:56 +0900

    Best Tab Fragment 완성

--- YamYam_Android/app/src/main/java/org/yamyamgoods/yamyam_android/BestGoodsFragment.kt
+++ YamYam_Android/app/src/main/java/org/yamyamgoods/yamyam_android/BestGoodsFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_best_goods.*
+import org.yamyamgoods.yamyam_android.util.TempData
 
 class BestGoodsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -24,55 +25,9 @@ class BestGoodsFragment : Fragment() {
         val ctx = activity!!.applicationContext
 
         rv_best_goods_frag_list.apply{
-            adapter = BestGoodsRVAdapter(ctx, tempData())
+            adapter = BestGoodsRVAdapter(ctx, TempData.bestGoods())
             layoutManager = GridLayoutManager(ctx, 2)
         }
     }
 
-    private fun tempData(): List<BestGoodsItem> {
-        val result = ArrayList<BestGoodsItem>()
-        result.add(
-                BestGoodsItem(
-                        0, false, R.drawable.img_goods1,
-                        "레드프린팅&프레스", "도무송 스티커",
-                        32900, 4.8, 10, 300)
-        )
-        result.add(
-                BestGoodsItem(
-                        0, true, R.drawable.img_goods2,
-                        "레드프린팅&프레스", "도무송 스티커",
-                        32900, 4.8, 10, 300)
-        )
-        result.add(
-                BestGoodsItem(
-                        0, true, R.drawable.img_goods3,
-                        "레드프린팅&프레스", "도무송 스티커",
-                        32900, 4.8, 10, 300)
-        )
-        result.add(
-                BestGoodsItem(
-                        0, false, R.drawable.img_goods4,
-                        "레드프린팅&프레스", "도무송 스티커",
-                        32900, 4.8, 10, 300)
-        )
-        result.add(
-                BestGoodsItem(
-                        0, false, R.drawable.img_goods1,
-                        "레드프린팅&프레스", "도무송 스티커",
-                        32900, 4.8, 10, 300)
-        )
-        result.add(
-                BestGoodsItem(
-                        0, true, R.drawable.img_goods2,
-                        "레드프린팅&프레스", "도무송 스티커",
-                        32900, 4.8, 10, 300)
-        )
-        result.add(
-                BestGoodsItem(
-                        0, false, R.drawable.img_goods3,
-                        "레드프린팅&프레스", "도무송 스티커",
-                        32900, 4.8, 10, 300)
-        )
-        return result
-    }
 }
