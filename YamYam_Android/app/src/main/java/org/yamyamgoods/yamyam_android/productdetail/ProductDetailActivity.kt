e43c49c8fa3c483cb4780a1e7bd6ce5b272a4db7
Malibin <nightmare73@naver.com>
2019-07-02 01:31:36 +0900

    중간 세이브 collaps Tool bar 평행하게 collaps 되는거 만들엇고 scrim 연구중....

--- YamYam_Android/app/src/main/java/org/yamyamgoods/yamyam_android/productdetail/ProductDetailActivity.kt
+++ YamYam_Android/app/src/main/java/org/yamyamgoods/yamyam_android/productdetail/ProductDetailActivity.kt
@@ -4,9 +4,12 @@ import android.graphics.Color
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_product_detail.*
 import org.yamyamgoods.yamyam_android.R
+import org.yamyamgoods.yamyam_android.util.px2dp
 
 /**
  * Created By Yun Hyeok
@@ -20,11 +23,14 @@ class ProductDetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_product_detail)
 
         setStatusBarTransparent()
+
+        viewInit()
+
+        test()
     }
 
     private fun setStatusBarTransparent() {
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-
         window.decorView.systemUiVisibility =
                 View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
                         View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
@@ -44,5 +50,38 @@ class ProductDetailActivity : AppCompatActivity() {
             winParams.flags = winParams.flags and bits.inv()
         }
         win.attributes = winParams
+    }
+
+    private fun viewInit() {
+        setImageCollapsingToolBar()
+    }
+
+    private fun setImageCollapsingToolBar() {
+        collapsing_toolbar_product_detail_act.apply {
+            contentScrim
+
+        }
+
+        val lp = iv_product_detail_act_image.layoutParams
+        lp.height = getDynamicImageHeight()
+
+        iv_product_detail_act_image.layoutParams = lp
+    }
+
+    private fun getDynamicImageHeight(): Int {
+        val displayMetrics = resources.displayMetrics
+        val phoneWidth = displayMetrics.widthPixels
+        return (phoneWidth * 321 / 360)
+    }
+
+    private fun test() {
+        val displayMetrics = resources.displayMetrics
+        val phoneWidth = displayMetrics.widthPixels
+        val phoneHeight = displayMetrics.heightPixels
+        Log.v("Malibin Debug", "widthpx : $phoneWidth, heightPx: $phoneHeight")
+
+        val phoneWidthDp = px2dp(phoneWidth, this)
+        val phoneHeightDp = px2dp(phoneHeight, this)
+        Log.v("Malibin Debug", "widthDp : $phoneWidthDp, heightDp: $phoneHeightDp")
     }
 }
