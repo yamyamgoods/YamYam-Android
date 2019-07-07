2c8878fafe6dd89d8904c8b00b36a607b027ec07
Malibin <nightmare73@naver.com>
2019-07-07 19:20:54 +0900

    로그인 요구 다이얼로그 레이아웃 완성

---
+++ YamYam_Android/app/src/main/java/org/yamyamgoods/yamyam_android/productdetail/dialog/LoginRequestDialog.kt
@@ -0,0 +1,19 @@
+package org.yamyamgoods.yamyam_android.productdetail.dialog
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import org.yamyamgoods.yamyam_android.R
+
+/**
+ * Created By Yun Hyeok
+ * on 7월 07, 2019
+ */
+
+class LoginRequestDialog(ctx: Context) : Dialog(ctx) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dialog_login_request)
+    }
+}
