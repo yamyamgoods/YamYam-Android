80dc5fe3128e497cdbd16c3538548c7d0fe9777c
Malibin <nightmare73@naver.com>
2019-07-09 05:10:53 +0900

    업체 액티비티 레이아웃 완성

---
+++ YamYam_Android/app/src/main/java/org/yamyamgoods/yamyam_android/storedetail/StoreDetailActivity.kt
@@ -0,0 +1,18 @@
+package org.yamyamgoods.yamyam_android.storedetail
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import org.yamyamgoods.yamyam_android.R
+
+/**
+ * Created By Yun Hyeok
+ * on 7월 09, 2019
+ */
+
+class StoreDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_store_detail)
+    }
+}
