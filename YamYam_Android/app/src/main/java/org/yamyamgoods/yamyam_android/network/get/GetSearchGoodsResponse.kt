a3cac1615e214ca52ad7c45beede9c5d5503a9bc
jeongeum <wjdma0103@naver.com>
2019-07-09 18:07:48 +0900

    GoodsData 하나로 통일

--- YamYam_Android/app/src/main/java/org/yamyamgoods/yamyam_android/network/get/GetSearchGoodsResponse.kt
+++ YamYam_Android/app/src/main/java/org/yamyamgoods/yamyam_android/network/get/GetSearchGoodsResponse.kt
@@ -1,23 +1,12 @@
 package org.yamyamgoods.yamyam_android.network.get
 
+import org.yamyamgoods.yamyam_android.network.GoodsData
+
 data class GetSearchGoodsResponse (
         val message: String,
-        val data: goods
+        val data: GoodsSearchResultList
 )
 
-data class goods(
-        val data: ArrayList<SearchGoodsData>
-)
-
-data class SearchGoodsData(
-        val search_after: List<Int>,
-        val goods_idx: Int,
-        val goods_name: String,
-        val goods_rating: Float,
-        val goods_price: Int,
-        val goods_minimum_amout: Int,
-        val goods_detail: String,
-        val goods_review_cnt: Int,
-        val goods_img: String,
-        val store_name: String
+data class GoodsSearchResultList(
+        val data: ArrayList<GoodsData>
 )
