30af5ff09322ad5a6fd28ebe517cff47f2bc6801
Lee Hyunjoo <winnerhj1109@naver.com>
2019-07-09 14:19:10 +0900

    네트워크 ApplicationController class를 object로 수정

--- YamYam_Android/app/src/main/java/org/yamyamgoods/yamyam_android/network/ApplicationController.kt
+++ YamYam_Android/app/src/main/java/org/yamyamgoods/yamyam_android/network/ApplicationController.kt
@@ -1,22 +1,15 @@
 package org.yamyamgoods.yamyam_android.network
 
-import android.app.Application
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class ApplicationController: Application() {
+object ApplicationController {
     private val baseURL = "http://13.209.245.84:3000/"
     lateinit var networkServiceGoods: NetworkServiceGoods
     lateinit var networkServiceStore: NetworkServiceStore
     lateinit var networkServiceUser: NetworkServiceUser
 
-    companion object{
-        lateinit var instance: ApplicationController
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        instance = this
+    init {
         buildNetwork()
     }
 
