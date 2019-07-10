f3d02753c974d09a0ecc39f0ae3c20773d7ca041
Lee Hyunjoo <winnerhj1109@naver.com>
2019-07-10 17:59:08 +0900

    유저 알람 목록보기 완료 & 닉네임 변경 부분 수정

---
+++ YamYam_Android/app/src/main/java/org/yamyamgoods/yamyam_android/network/get/GetAlarmListResponse.kt
@@ -0,0 +1,15 @@
+package org.yamyamgoods.yamyam_android.network.get
+
+data class GetAlarmListResponse(
+        val message: String,
+        val data: ArrayList<AlarmListData>
+)
+
+data class AlarmListData(
+        val alarm_idx: Int,
+        val alarm_target_idx: Int,
+        val alarm_check_flag: Int,
+        val alarm_date_time: String,
+        val alarm_message: String,
+        val goods_review_idx: Int
+)
--- YamYam_Android/app/src/main/java/org/yamyamgoods/yamyam_android/network/put/PutMypageEditProfileImageRequest.kt
+++ YamYam_Android/app/src/main/java/org/yamyamgoods/yamyam_android/network/put/PutMypageEditProfileImageRequest.kt
@@ -3,5 +3,5 @@ package org.yamyamgoods.yamyam_android.network.put
 import okhttp3.MultipartBody
 
 data class PutMypageEditProfileImageRequest (
-        val img: MultipartBody.Part
+        var message: String
 )
