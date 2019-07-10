9928c02d89122aa8e291928d359946f7cb244197
Malibin <nightmare73@naver.com>
2019-07-11 03:37:23 +0900

    스토어 랭킹 서버연동 카테고리 받아오는거함

---
+++ YamYam_Android/app/src/main/java/org/yamyamgoods/yamyam_android/home/store/ranking/StoreRankingContract.kt
@@ -0,0 +1,28 @@
+package org.yamyamgoods.yamyam_android.home.store.ranking
+
+import org.yamyamgoods.yamyam_android.dataclass.StoreCategory
+import org.yamyamgoods.yamyam_android.util.BasePresenter
+import org.yamyamgoods.yamyam_android.util.BaseView
+
+/**
+ * Created By Yun Hyeok
+ * on 7월 11, 2019
+ */
+
+interface StoreRankingContract {
+
+    interface View : BaseView<Presenter> {
+
+        fun showServerFailToast(message: String, t: Throwable)
+
+        fun setStoreCategory(data: List<StoreCategory>)
+
+    }
+
+    interface Presenter : BasePresenter {
+
+        fun getStoreCategory()
+
+        fun getStoreRankingList(storeCategoryIdx: Int, lastIdx: Int = -1)
+    }
+}
--- YamYam_Android/app/src/main/java/org/yamyamgoods/yamyam_android/home/store/ranking/StoreRankingFragment.kt
+++ YamYam_Android/app/src/main/java/org/yamyamgoods/yamyam_android/home/store/ranking/StoreRankingFragment.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
+import android.text.TextUtils
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.fragment_store_ranking.*
+import org.jetbrains.anko.toast
 import org.yamyamgoods.yamyam_android.R
+import org.yamyamgoods.yamyam_android.dataclass.StoreCategory
 import org.yamyamgoods.yamyam_android.home.store.ranking.adapter.StoreRankingRVAdapter
+import org.yamyamgoods.yamyam_android.network.ApplicationController
 import org.yamyamgoods.yamyam_android.util.TempData
 
-class StoreRankingFragment : Fragment() {
+class StoreRankingFragment : Fragment(), StoreRankingContract.View {
+
+    override lateinit var presenter: StoreRankingContract.Presenter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_store_ranking, container, false)
@@ -21,11 +29,41 @@ class StoreRankingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        presenterInit()
+
+        getServerData()
         viewInit()
     }
 
+    override fun showServerFailToast(message: String, t: Throwable) {
+        activity!!.toast(message)
+        Log.v("Malibin Debug", "t : ${t.message}, stack : ${TextUtils.join("\n", t.stackTrace)}")
+    }
+
+    override fun setStoreCategory(data: List<StoreCategory>) {
+        categorySpinnerInit()
+    }
+
+    private fun presenterInit() {
+        presenter = StoreRankingPresenter().apply {
+            userToken =
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWR4IjoxLCJpYXQiOjE1NjIzMTUzNjYsImV4cCI6MTU2MzYyOTM2Nn0.ZkDGasoDPHTrGvy7yFOT9cPjTQ7gnnUOqekY_zYrAuc"
+            view = this@StoreRankingFragment
+            storeRepository = ApplicationController.networkServiceStore
+        }
+    }
+
+    private fun getServerData() {
+        presenter.getStoreCategory()
+    }
+
+    private fun categorySpinnerInit() {
+        //spinner_rv_item_store_ranking_frag.adapter = ArrayAdapter(this,)
+    }
+
+
     private fun viewInit() {
-        rv_store_ranking_frag_list.apply {
+        rv_item_store_ranking_frag_list.apply {
             val ctx = activity!!.applicationContext
             adapter = StoreRankingRVAdapter(ctx, TempData.storeRankings())
             layoutManager = LinearLayoutManager(ctx)
